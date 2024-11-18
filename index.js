d61ef0acd7d2d3fa985394c414a4ed5d7b2c9a85
ubhalerao <ubhalerao@medisticshealth.com>
2024-11-18 12:58:18 +0530

    port added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import dotenv from 'dotenv';
 dotenv.config({ path: './.env' });
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 
 app.use(express.json());
