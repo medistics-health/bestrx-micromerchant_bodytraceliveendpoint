80b9572038efbd10e0f53bc19c1fe4407c66d537
Medistics Health <147605779+ubhalerao@users.noreply.github.com>
2025-11-04 13:17:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ app.get("/",async (req,res)=>{
 	res.send("runeed successfully")
 })
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+// app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}`);
+// });
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${port}`);
 });
